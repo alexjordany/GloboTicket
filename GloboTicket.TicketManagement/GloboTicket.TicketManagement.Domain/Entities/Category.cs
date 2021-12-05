b6838a97ec5ac1c48b28f11a330ca976040e1aa4
Alex Jordany Parrales López <alexjordany@outlook.com>
2021-12-05 17:47:14 -0600

    Adding the application domain

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace GloboTicket.TicketManagement.Domain.Entities;
+
+public class Category : AuditableEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } 
+    public ICollection <Event> Events { get; set; }
+}
---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Domain/Entities/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace GloboTicket.TicketManagement.Domain.Entities;
+
+public class Event : AuditableEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public String Artist { get; set; }
+    public DateOnly Date { get; set; }
+    public string? Description { get; set; }
+    public string ImageURL { get; set; }
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+
+
+
+}
